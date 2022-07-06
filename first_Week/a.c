c65b58f2917038adbf52fcf78678ea0c35e1d113
Davi do Nascimento Santos <davinascimento19072001@gmail.com>
2022-07-05 21:46:06 -0300

    Add solution of letter a of second question

--- first_Week/a.c
+++ first_Week/a.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
-#include <math.h>
+
 int main(){
 	double raio, altura, volume_esfera, volume_cone, volume_cilindro;
-	printf("Digite o raio: \n");
+	printf("Digite o raio: ");
 	scanf("%lf", &raio);
-	printf("Digite a altura: \n");
+	printf("Digite a altura: ");
 	scanf("%lf", &altura);
-	volume_esfera = (4 * 3.14 * pow(raio, 3)) / 3;
+	volume_esfera = (4 * 3.14 * (raio * raio * raio)) / 3;
 	printf("O volume da esfera é %lf\n", volume_esfera);
+	volume_cone = (3.14 * (raio * raio) * altura) / 3;
+	printf("O volume do cone é %lf\n", volume_cone);
+	volume_cilindro = 3.14 * (raio * raio) * altura;
+	printf("O volume do cilindro é %lf\n", volume_cilindro);
 	return 0;	
 	
 }
